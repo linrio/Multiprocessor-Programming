bb34670b295c10889b2167f0845111238231b33b
Lingfeng Lin <ilinlingfeng@gmail.com>
2016-12-12 00:25:36 +0800

    Add files via upload

---
+++ TicketingSystem.java
@@ -0,0 +1,16 @@
+package ticketingsystem;
+
+class Ticket{
+	long tid;         //车票编号
+	String passenger; //乘客名字
+	int route;        //列车车次
+	int coach;        //车厢号 
+	int seat;         //座位号
+	int departure;    //出发站编号
+	int arrival;      //到达站编号
+}
+public interface TicketingSystem {
+	Ticket buyTicket(String passenger, int route, int departure, int arrival);
+	int inquiry(int route, int departure, int arrival);
+	boolean refundTicket(Ticket ticket);
+}
